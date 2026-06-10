6d8c7a973f84d84049cc2416d8591017976b000f
marco <email@marcobeierer.com>
2026-06-10 14:51:21 +0200

    added test urls

--- config.go
+++ config.go
@@ -1,5 +1,6 @@
 package main
 
-//var endpoint = "http://localhost:9999/sitemap/v2/"
-
 var endpoint = "https://api.marcobeierer.com/sitemap/v2/"
+
+// endpoint = "https://apiserver-test.mxbe.ch/sitemap/v2/"
+// endpoint = "http://localhost:9999/sitemap/v2/"
